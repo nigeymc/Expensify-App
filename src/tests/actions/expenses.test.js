4321cf10a232e372cd172845d37e8fc22b082c5e
nigeymc <mckenna.niall@gmail.com>
2020-11-13 16:35:10 +0000

    setting up separate expensify app

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,52 @@
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('should set up remove expense action argument', () => {
+    const action = removeExpense({ id: '123abc' });
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
+});
+
+test('should set up edit expense action argument', () => {
+    const action = editExpense('123abc', { note: 'New note value' });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: '123abc',
+        updates: { note: 'New note value' }
+    });
+});
+
+test('should set up add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'Rent',
+        amount: 109500,
+        createdAt: 1000,
+        note: 'This was last months rent'
+    }
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    });
+});
+
+test('should set up add expense action object with default values', () => {
+    const expenseData = {
+        description: '',
+        amount: 0,
+        createdAt: 0,
+        note: ''
+    }
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    });
+});
