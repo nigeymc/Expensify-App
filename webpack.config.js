470e2a453b5f5b97e7eea4d29e404da6b8806a04
nigeymc <mckenna.niall@gmail.com>
2020-11-16 15:47:28 +0000

    Set up devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,11 @@ module.exports = (env) => {
   return {
     entry: "./src/app.js",
     output: {
-      path: path.join(__dirname, "public"),
+      path: path.join(__dirname, "public", 'dist'),
       filename: "bundle.js",
     },
     module: {
-      rules: [
-        {
+      rules: [{
           loader: "babel-loader",
           test: /\.js$/,
           exclude: /node_modules/,
@@ -21,8 +20,7 @@ module.exports = (env) => {
         {
           test: /\.s?css$/,
           use: CSSExtract.extract({
-            use: [
-              {
+            use: [{
                 loader: 'css-loader',
                 options: {
                   sourceMap: true
@@ -46,6 +44,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, "public"),
       historyApiFallback: true,
+      publicPath: '/dist/'
     }
   };
 };
